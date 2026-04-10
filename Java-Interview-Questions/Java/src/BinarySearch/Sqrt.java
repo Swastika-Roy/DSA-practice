8e4afbfc9bd51f1127449ba94146a07b8469f294
Swastika Roy <swastikaroytitu@gmail.com>
2026-04-10 23:31:29 +0530

    done

--- Java-Interview-Questions/Java/src/BinarySearch/Sqrt.java
+++ Java-Interview-Questions/Java/src/BinarySearch/Sqrt.java
@@ -2,24 +2,24 @@ package BinarySearch;
 
 public class Sqrt {
     public static int mySqrt(int x) {
-        long st = 0; long end = x;
-        long ans = 0;
-        while(st <= end){
+        long st=0; long end=x;
+        long ans=0;
+        while (st <= end){
             long mid = st+(end-st)/2;
-            if(mid*mid == x)return (int)mid;
+            if (mid*mid == x)return (int)mid;
             else if(mid*mid < x){
-                st = mid +1;
-                ans=mid;
+                st = mid+1;
+                ans =mid;
             }
-            else{
-                end =mid-1;
+            else {
+                end = mid - 1;
             }
         }
-        return (int) ans;
+        return (int)ans;
     }
 
     public static void main(String[] args) {
-        int x = 8;
+        int x = 16;
         System.out.println(mySqrt(x));
 
     }
--- Java-Interview-Questions/Java/src/InsertionSort.java
+++ Java-Interview-Questions/Java/src/InsertionSort.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void insertionSort(int[] arr){
-        for (int i=0;i <= arr.length-1;i++){
+        for(int i=0;i<=arr.length-1;i++){
             int j=i;
-            while(j > 0 && arr[j-1] > arr[j]){
+            while (j > 0 && arr[j-1]>arr[j]){
                 int temp = arr[j];
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
@@ -18,3 +18,13 @@ public class InsertionSort {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+//for (int i=0;i <= arr.length-1;i++){
+//int j=i;
+//            while(j > 0 && arr[j-1] > arr[j]){
+//int temp = arr[j];
+//arr[j] = arr[j-1];
+//arr[j-1] = temp;
+//j--;
+//        }
+//        }
