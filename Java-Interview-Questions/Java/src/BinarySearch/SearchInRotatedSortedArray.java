f66d04f639b2ac18d64f93328d2f6ebbd3e01345
Swastika Roy <swastikaroytitu@gmail.com>
2026-04-12 13:03:57 +0530

    done

--- Java-Interview-Questions/Java/src/BinarySearch/SearchInRotatedSortedArray.java
+++ Java-Interview-Questions/Java/src/BinarySearch/SearchInRotatedSortedArray.java
@@ -2,38 +2,63 @@ package BinarySearch;
 
 public class SearchInRotatedSortedArray {
     public static boolean search(int[] arr, int target) {
-        int st = 0 , end = arr.length-1;
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(arr[mid]==target)return true;
-            //checking if duplicate
-            if(arr[mid]==arr[st] && arr[mid] == arr[end]){
-                st++;
-                end--;
-            }
-            else if(arr[mid]<=arr[end]){
-                if(arr[mid]<target && arr[end]>=target){
-                    st = mid+1;
-                }else{
-                    end = mid-1;
-                }
-            }else{
-                if(arr[mid]>target && arr[st]<=target){
-                    end = mid-1;
-
-                }else{
-                    st = mid+1;
-                }
-            }
-
-        }
-        return false;
+      int st = 0 ; int end = arr.length-1;
+      while (st <=end){
+          int mid = st+(end-st)/2;
+          if(arr[mid] == target)return true;
+           if (arr[mid] == arr[st] && arr[mid]==arr[end]){
+              st++;
+              end--;
+          }
+           else if (arr[mid]<=end){
+               if(arr[mid]<target && target<=arr[end]){
+                   st=mid+1;
+               }else {
+                   end=mid-1;
+               }
+           }
+           else {
+               if(arr[mid]>target && arr[st]<=target){
+                   end=mid-1;
+               }else {
+                   st=mid+1;
+               }
+           }
+      }
+      return false;
 
     }
 
     public static void main(String[] args) {
         int[] arr = {2,5,6,0,0,1,2};
-        int target = 0;
+        int target = 1;
         System.out.println(search(arr,target));
     }
 }
+
+//  int st = 0 , end = arr.length-1;
+//        while(st<=end){
+//            int mid = st+(end-st)/2;
+//            if(arr[mid]==target)return true;
+//            //checking if duplicate
+//            if(arr[mid]==arr[st] && arr[mid] == arr[end]){
+//                st++;
+//                end--;
+//            }
+//            else if(arr[mid]<=arr[end]){
+//                if(arr[mid]<target && arr[end]>=target){
+//                    st = mid+1;
+//                }else{
+//                    end = mid-1;
+//                }
+//            }else{
+//                if(arr[mid]>target && arr[st]<=target){
+//                    end = mid-1;
+//
+//                }else{
+//                    st = mid+1;
+//                }
+//            }
+//
+//        }
+//        return false;
