863624539962d813451a3383c9fdb708cf0e1e96
Swastika Roy <swastikaroytitu@gmail.com>
2026-04-21 23:38:31 +0530

    done

--- Java-Interview-Questions/Java/src/removeSpacesFromAString.java
+++ Java-Interview-Questions/Java/src/removeSpacesFromAString.java
@@ -1,12 +1,12 @@
 public class removeSpacesFromAString {
     public static String removeSpace(String s){
-        StringBuilder str = new StringBuilder();
-        for (int i = 0 ; i<s.length() ; i++){
-            if(s.charAt(i)!=' '){
-                str.append(s.charAt(i));
-            }
-        }
-        return str.toString();
+       StringBuilder sb = new StringBuilder();
+       for(int i=0;i < s.length();i++){
+           if(s.charAt(i) != ' '){
+               sb.append(i);
+           }
+       }
+       return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -14,3 +14,11 @@ public class removeSpacesFromAString {
         System.out.println(removeSpace(str));
     }
 }
+
+//StringBuilder str = new StringBuilder();
+//        for (int i = 0 ; i<s.length() ; i++){
+//        if(s.charAt(i)!=' '){
+//        str.append(s.charAt(i));
+//        }
+//        }
+//        return str.toString();
