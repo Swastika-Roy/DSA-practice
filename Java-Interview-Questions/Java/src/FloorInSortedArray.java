15eadab88c35bbb965e974c322c5b7b288790a93
Swastika Roy <swastikaroytitu@gmail.com>
2026-04-26 13:00:23 +0530

    done

---
+++ Java-Interview-Questions/Java/src/FloorInSortedArray.java
@@ -0,0 +1,21 @@
+public class FloorInSortedArray {
+    public static int findFloor(int[] arr, int x) {
+        int st = 0; int end = arr.length-1;int ans =-1;
+        while(st <= end){
+            int mid = st+(end-st)/2;
+            if(arr[mid] <= x){
+                ans = mid;
+                st = mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int x = 5;
+        System.out.println(findFloor(arr,x));
+    }
+}
---
+++ Java-Interview-Questions/Java/src/KthSmallest.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthSmallest {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int k = 3;
+        int ans = kthSmallest(arr,k);
+        System.out.println(ans);
+    }
+
+    public static int kthSmallest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i < arr.length;i++){
+            pq.add(arr[i]);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+
+}
