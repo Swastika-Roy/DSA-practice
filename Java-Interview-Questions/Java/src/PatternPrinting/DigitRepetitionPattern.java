16406ae3bd690f57e1bb92dae77d0287b35e0e36
Swastika Roy <swastikaroytitu@gmail.com>
2026-05-06 01:44:24 +0530

    done

--- Java-Interview-Questions/Java/src/PatternPrinting/DigitRepetitionPattern.java
+++ Java-Interview-Questions/Java/src/PatternPrinting/DigitRepetitionPattern.java
@@ -1,18 +1,26 @@
 package PatternPrinting;
 
 public class DigitRepetitionPattern {
-    public static void DigitRepetitionPatternPrint(int n){
+    public static void DigitRepetitionPatternPrint(int n) {
         int count = 0;
-        for (int i = 1 ; i<=n ; i++){
+        for (int i = 1; i <= n; i++) {
             count++;
-            for (int j = 1 ; j<=i ; j++){
-                System.out.print(count+" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
             }
             System.out.println();
         }
     }
-
     public static void main(String[] args) {
         DigitRepetitionPatternPrint(5);
     }
 }
+
+//int count = 0;
+//        for (int i = 1 ; i<=n ; i++){
+//count++;
+//        for (int j = 1 ; j<=i ; j++){
+//        System.out.print(count+" ");
+//            }
+//                    System.out.println();
+     //   }
--- Java-Interview-Questions/Java/src/PatternPrinting/NumberChangingPyramid.java
+++ Java-Interview-Questions/Java/src/PatternPrinting/NumberChangingPyramid.java
@@ -2,17 +2,33 @@ package PatternPrinting;
 
 public class NumberChangingPyramid {
     public static void NumberChangingPyramidPrint(int n){
-        int count = 0;
-        for (int i = 1 ; i<=n ; i++){
-            for (int j = 1 ; j<=i ; j++){
-                count++;
-                System.out.print(count+" ");
-            }
-            System.out.println();
-        }
+int count = 0;
+for(int i=1; i <= n; i++){
+    for(int j=1;j<=i;j++){
+        count++;
+        System.out.print(count+" ");
+    }
+    System.out.println();
+}
     }
 
     public static void main(String[] args) {
-        NumberChangingPyramidPrint(5);
+        NumberChangingPyramid.NumberChangingPyramidPrint(5);
     }
 }
+
+
+
+
+
+
+
+//int count = 0;
+//        for (int i = 1 ; i<=n ; i++){
+//        for (int j = 1 ; j<=i ; j++){
+//count++;
+//        System.out.print(count+" ");
+//            }
+//                    System.out.println();
+
+    //}
